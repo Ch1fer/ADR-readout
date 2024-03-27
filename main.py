fdf2605e952893fbd48a4fb85e2000d728646b37
Ch1fer <evgengajdas@gmail.com>
2024-03-27 21:17:48 +0100

    add model(not worked) add training loop

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import torch
 from torch.utils import data
 from torch.utils.data import Dataset
-import torch as nn
+import torch.nn as nn
 import pandas as pd
-import numpy as np
 import torchvision as tv
 import cv2 as cv
 import os
 import matplotlib.pyplot as plt
-from PIL import Image
 
 
 
@@ -71,7 +69,7 @@ class CustomDataset(Dataset):  # My custom dataset class
 
 image_dir = 'data/images'
 label_dir = 'data/label.csv'
-countsOfRows = 100
+countsOfRows = 1000
 
 
 """___PREPARE_DATA_AND_TRANSFORM___"""
@@ -86,26 +84,25 @@ for file in files:
     if file.endswith('.jpg'):
         image_path = os.path.join(image_dir, file)
         img = cv.imread(image_path)
-        img = cv.resize(img, (64, 64), interpolation=cv.INTER_AREA)  # resize image
+        img = cv.resize(img, (48, 48), interpolation=cv.INTER_AREA)  # resize image
 
-        clahe = cv.createCLAHE(clipLimit=0.05, tileGridSize=(4, 4))  # increase image contrast
+        clahe = cv.createCLAHE(clipLimit=0.1, tileGridSize=(4, 4))  # increase image contrast
         b, g, r = cv.split(img)
         img = cv.merge((clahe.apply(b), clahe.apply(g), clahe.apply(r)))
 
         img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)  # transform images to gray scale, only one channel of gray
         images.append(img)
+        # plt.imshow(img, cmap="gray")
+        # plt.show()
+
 
 labels = pd.read_csv(label_dir, nrows=countsOfRows)  # Load labels
-labels = torch.tensor(labels.values, dtype=torch.int64)
+labels = torch.tensor(labels.values, dtype=torch.float32)
 
 transform = tv.transforms.ToTensor()
 myDataset = CustomDataset(images, labels, transform=transform)  # Create custom dataset from previously images & labels
 
 show_8_samples(myDataset, countsOfRows, 0)  # displays 8 images starting from the index "start"
-
-
-
-
 
 
 # plt.imshow(myDataset[row][0].numpy()[0], cmap="gray")  # picture
@@ -124,8 +121,56 @@ for img, l in dataloader:
 
 
 # """___MODEL___"""
-# class MyModel(nn.Module):
-#     def __init__(self):
-#         super().__init__()
 #
-#         linear1 = nn.Linear(3 * 300 * 300, )  # 270k :)
+#
+# class ShallowNetwork(nn.Module):
+#     def __init__(self):
+#         super(ShallowNetwork, self).__init__()
+#
+#         self.flatten = nn.Flatten()
+#         self.linear1 = nn.Linear(48 * 48, 100)
+#         self.linear2 = nn.Linear(100, 50)
+#         self.linear3 = nn.Linear(50, 2)
+#         self.act = nn.ReLU()
+#         self.outF = nn.Softmax()
+#
+#     def forward(self, x):
+#         x = self.flatten(x)
+#         x = self.linear1(x)
+#         x = self.act(x)
+#         x = self.linear2(x)
+#         x = self.act(x)
+#         x = self.linear3(x)
+#         x = self.outF(x)
+#         return x
+#
+#
+# learning_rate = 0.01
+# model = ShallowNetwork()
+# loss_fn = nn.CrossEntropyLoss()
+# optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+#
+#
+#
+# """___TRAINING___"""
+# epochs = 100
+#
+# for epoch in range(epochs):
+#     loss_val = 0
+#     for img, label in dataloader:
+#         optimizer.zero_grad()
+#
+#         # one_hot_first_column = nn.functional.one_hot(label[:, 0], 12)
+#         # one_hot_second_column = nn.functional.one_hot(label[:, 1], 60)
+#         #
+#         # one_hot_label = torch.cat((one_hot_first_column, one_hot_second_column), dim=1)
+#         predict = model(img)
+#
+#         loss = loss_fn(predict, label)
+#         loss.backward()
+#
+#         loss_val += loss.item()
+#
+#         optimizer.step()
+#
+#     print(loss_val / len(dataloader))
