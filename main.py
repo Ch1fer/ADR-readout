6661a926ce4ff33701ebf8c6cf4edfbe3f7d3d84
Dandelion <380portal2@gmail.com>
2024-05-13 22:14:57 +0200

    merge

--- main.py
+++
@@ -1,9 +0,0 @@
-from NN_analog_clock_reader import predict_clock as pc
-from photo_preprocessing import preprocessing 
-
-file_path = "photo_preprocessing/assets/clock4.jpg"
-preprocessing.draw(file_path)
-
-output_image = preprocessing.draw(file_path)
-
-pc.predict(file_path)
